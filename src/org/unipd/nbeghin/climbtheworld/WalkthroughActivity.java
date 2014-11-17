89fd4d3c76616b7ef583f8c737f7b1370b9c7424
silviagreen <segato.silvia.job@gmail.com>
2014-11-17 18:40:36 +0100

    Migliorato demo e reso multi lingua

--- src/org/unipd/nbeghin/climbtheworld/WalkthroughActivity.java
+++ src/org/unipd/nbeghin/climbtheworld/WalkthroughActivity.java
@@ -2,23 +2,29 @@ package org.unipd.nbeghin.climbtheworld;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CirclePageIndicator;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class WalkthroughActivity extends Activity {
 
-    private static final int MAX_VIEWS = 3;
+    private static final int MAX_VIEWS = 4;
 
     ViewPager mViewPager;
-    TextView nav;
+    //TextView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +33,15 @@ public class WalkthroughActivity extends Activity {
         setContentView(R.layout.activity_walkthrough);
         mViewPager = (ViewPager) findViewById(R.id.view_pager);
         mViewPager.setAdapter(new WalkthroughPagerAdapter());
+        WalkthroughPageChangeListener listener = new WalkthroughPageChangeListener();
+        mViewPager.setOnPageChangeListener(listener);
+//        nav = (TextView) findViewById(R.id.screen_navigation_button);
+//        nav.setText("Page 1 of " + MAX_VIEWS);
         mViewPager.setOnPageChangeListener(new WalkthroughPageChangeListener());
-        nav = (TextView) findViewById(R.id.screen_navigation_button);
-        nav.setText("Page 1 of " + MAX_VIEWS);
-        mViewPager.setOnPageChangeListener(new WalkthroughPageChangeListener());
+        
+        CirclePageIndicator cri= (CirclePageIndicator)findViewById(R.id.indicator);
+        cri.setViewPager(mViewPager);
+        cri.setOnPageChangeListener(listener);
     }
     
     @Override
@@ -63,6 +74,7 @@ public class WalkthroughActivity extends Activity {
             Log.e("walkthrough", "instantiateItem(" + position + ");");
             LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View imageViewContainer = inflater.inflate(R.layout.walkthrough_page, null);
+            Typeface tf = Typeface.createFromAsset(ClimbApplication.getContext().getAssets(),"fonts/sketch-me.ttf");  
             ImageView imageView1 = (ImageView) imageViewContainer.findViewById(R.id.imageView1);
             ImageView imageView2 = (ImageView) imageViewContainer.findViewById(R.id.imageView2);
             ImageView imageView3 = (ImageView) imageViewContainer.findViewById(R.id.imageView3);
@@ -70,43 +82,96 @@ public class WalkthroughActivity extends Activity {
             TextView text1 = (TextView) imageViewContainer.findViewById(R.id.textView2);
             TextView text2 = (TextView) imageViewContainer.findViewById(R.id.textView3);
             TextView text3 = (TextView) imageViewContainer.findViewById(R.id.textView4);
+            TextView intro = (TextView) imageViewContainer.findViewById(R.id.textIntro);
+            title.setTypeface(tf);
+            text1.setTypeface(tf);
+            text2.setTypeface(tf);
+            text3.setTypeface(tf);
+            intro.setTypeface(tf);
+            intro.setVisibility(View.INVISIBLE);
             
             switch(position) {
             case 0:
-            		title.setText("How it works (also offline)");
-                imageView1.setImageResource(R.drawable.phone);
-                text1.setText("1. Choose a building to climb");
-                imageView2.setImageResource(R.drawable.man_stairs);
-                text2.setText("2. Pick yout phone with you and make stairs");
-                imageView3.setImageResource(R.drawable.lock_win);
-                text3.setText("3. Win points, badge....and glory!!!!");
+            	intro.setVisibility(View.INVISIBLE);
+            	title.setText(getString(R.string.title_1));
+                imageView1.setImageResource(R.drawable.cards);
+                imageView1.setPadding(0, 0, 15, 0);
+//                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+//                lp.setMargins(0, 0, 2, 0);
+//                imageView1.setLayoutParams(lp);
+                text1.setText(getString(R.string.text_1_1));
+                imageView2.setImageResource(R.drawable.stairs);
+                
+                text2.setText(getString(R.string.text_1_2));
+                int i = (String.valueOf(text2.getText())).indexOf("p1");
+        		SpannableString ss = new SpannableString(text2.getText()); 
+                Drawable d = getResources().getDrawable(R.drawable.play); 
+                d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight()); 
+                ImageSpan span = new ImageSpan(d, ImageSpan.ALIGN_BASELINE); 
+                ss.setSpan(span, i, i+"p1".length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE); 
+                text2.setText(ss); 
+
+                imageView3.setImageResource(R.drawable.climb);
+                text3.setText(getString(R.string.text_1_3));
                 break;
 
             case 1:
-            		title.setText("Connect to facebook");
+            	intro.setVisibility(View.VISIBLE);
+            		title.setText(getString(R.string.title_2));
+            		intro.setText(getString(R.string.intro_2));
                 imageView1.setImageResource(R.drawable.help_friend);
-                text1.setText("You can ask your friends to help you climbing the building faster");
+                imageView1.setPadding(30, 30, 30, 30);
+                text1.setText(getString(R.string.text_2_1));
                 imageView2.setImageResource(R.drawable.competition);
-                text2.setText("Challenge you friends: who will arrive first????");
+                imageView2.setPadding(0, 0, 60, 0);
+                text2.setText(getString(R.string.text_2_2));
                 imageView3.setImageResource(R.drawable.team);
-                text3.setText("Pick a challenger, create a team to reach the top first!!!!");
+                text3.setText(getString(R.string.text_2_3));
 
                 break;
 
             case 2:
-            		title.setText("Monitor your results");
-            		imageView1.setImageResource(R.drawable.stats);
-            		text1.setText("See your profile and progress in the game");
-            		imageView2.setImageResource(R.drawable.heart);
-            		text2.setText("Get fit with our stairs counter");
-            		imageView3.setImageResource(R.drawable.cup);
-            		text3.setText("Admire your throphies!!!!");
+            	intro.setVisibility(View.INVISIBLE);
+            		title.setText(getString(R.string.title_3));
+            		imageView1.setImageResource(R.drawable.swipe0);
+            		text1.setText(getString(R.string.text_3_1));
+            		imageView2.setImageResource(R.drawable.swipe1);
+            		text2.setText(getString(R.string.text_3_2));
+            		imageView3.setImageResource(R.drawable.swipe2);
+            		text3.setText(getString(R.string.text_3_3));
 
                 break;
 
-//            case 3:
-//             //   imageView.setImageResource(R.drawable.competition);
-//                break;
+            case 3:
+            	intro.setVisibility(View.INVISIBLE);
+            	title.setText(getString(R.string.title_4));
+        		imageView1.setImageResource(R.drawable.stats);
+        		imageView1.setPadding(10, 10, 10, 10);
+        		text1.setText(getString(R.string.text_4_1));
+        		
+                i = (String.valueOf(text1.getText())).indexOf("p2");
+        		ss = new SpannableString(text1.getText()); 
+                d = getResources().getDrawable(R.drawable.profile); 
+                d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight()); 
+                span = new ImageSpan(d, ImageSpan.ALIGN_BASELINE); 
+                ss.setSpan(span, i, i+"p2".length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE); 
+                text1.setText(ss); 
+        		
+        		imageView2.setImageResource(R.drawable.heart); 
+        		imageView2.setPadding(50, 50, 50, 50);
+        		text2.setText(getString(R.string.text_4_2));
+        		ss = new SpannableString(text2.getText()); 
+        		i = (String.valueOf(text2.getText())).indexOf("p2");
+        		 d = getResources().getDrawable(R.drawable.profile); 
+        		 d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight()); 
+                 span = new ImageSpan(d, ImageSpan.ALIGN_BASELINE); 
+                ss.setSpan(span, i, i+"p2".length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE); 
+                text2.setText(ss);
+        		
+        		imageView3.setImageResource(R.drawable.settigs);
+        		imageView3.setPadding(20, 20, 20, 20);
+        		text3.setText(getString(R.string.text_4_3));
+                break;
 //
 //            case 4:
 //             //   imageView.setImageResource(R.drawable.help_friend);
@@ -149,7 +214,7 @@ public class WalkthroughActivity extends Activity {
 
             }
             
-            nav.setText("Page " + (position + 1) + " of " + MAX_VIEWS);
+          // nav.setText("Page " + (position + 1) + " of " + MAX_VIEWS);
 
 
         }
